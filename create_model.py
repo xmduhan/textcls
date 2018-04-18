02713058f2ec5b814fe324c3580de5979882f6be
xmduhan <xmduhan@gmail.com>
2018-04-18 11:45:35 +0800

    ...

--- create_model.py
+++ create_model.py
@@ -3,15 +3,14 @@
 
 from __future__ import division
 
-import os
 import pickle
 
-import pandas as pd
 import tensorflow as tf
 
-from collections import Counter
-
 from prompt_toolkit import prompt
+
+import config
+from helper import classes, vocabulary
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -20,32 +19,16 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 def main():
     """ """
     # Prompt for confirm
-    prompt_text = u'Create model will delete model that already exists (y/n)?'
+    prompt_text = u'Create model will delete model that already exists (y/n)?:'
     if prompt(prompt_text).lower().strip() != 'y':
         print u'quit ...'
         return
-
-    # Load train data
-    train = pd.read_csv('data/train.csv', encoding='utf-8')
-
-    # Generate vocabulary
-    counter = Counter(''.join(list(train['content'])))
-    vocabulary = pd.Series(counter).sort_values(ascending=False).reset_index()
-    vocabulary.columns = ['char', 'count']
-    vocabulary.to_csv('model/vocabulary.csv', encoding='utf-8')
-
-    # Generate classes
-    classes = train['class'].drop_duplicates()
-    classes = classes.reset_index(drop=True)
-    classes.columns = ['class']
-    classes.to_csv('model/classes.csv', encoding='utf-8')
 
     # Define the graph
     graph = tf.Graph()
     with graph.as_default():
         # Placenode for data feeding
-        # seq_length = 600
-        x = tf.placeholder(tf.int32, [None, 600])
+        x = tf.placeholder(tf.int32, [None, config.max_length])
         y = tf.placeholder(tf.float32, [None, len(classes)])
         keep_prob = tf.placeholder(tf.float32)
 
@@ -80,14 +63,11 @@ def main():
         correct = tf.equal(tf.argmax(x, 1), prediction)
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-        model_path = 'model'
-        model_filename = 'model/model'
-
     # Save model for using later
     with tf.Session(graph=graph) as session:
         session.run(tf.global_variables_initializer())
         saver = tf.train.Saver(max_to_keep=1)
-        saver.save(session, model_filename)
+        saver.save(session, config.model_filename)
 
     nodes = {
         'x': x.name,
@@ -98,8 +78,7 @@ def main():
         'optimizer': optimizer.name,
         'accuracy': accuracy.name,
     }
-    nodes_file_name = os.path.join(model_path, 'nodes.pk')
-    pickle.dump(nodes, open(nodes_file_name, 'wb'))
+    pickle.dump(nodes, open(config.nodes_filename, 'wb'))
 
 
 if __name__ == "__main__":
